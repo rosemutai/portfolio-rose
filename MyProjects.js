e161409c5863291eba75d13890a2530a7743d63a
rosemutai <chepngetichrose2030@gmail.com>
2023-04-09 22:08:23 +0300

    Add projects array

---
+++ MyProjects.js
@@ -0,0 +1,67 @@
+const MyProjects = [
+  {
+    name: 'To-Do-List',
+    experience: {
+      company: 'Microverse',
+      role: 'Front End Dev',
+      year: 2023,
+    },
+    description: 'Add and remove your daily list of todo tasks here.',
+    featuredImage: './src/assets/SnapshootPortfolio.svg',
+    technologies: [
+      'html', 'css', 'javascript',
+    ],
+    liveDemo: 'https://rosemutai.github.io/My-Portfolio/',
+    sourceCode: 'https://rosemutai.github.io/My-Portfolio/'
+  },
+
+  {
+    name: 'Multi-Post Stories',
+    experience: {
+      company: 'canopy',
+      role: 'Back End Dev',
+      year: 2015,
+    },
+    description: 'A daily selection of privately personalized reads no accounts or sign-ups required.',
+    featuredImage: './src/assets/SnapshootPortfolio1.svg',
+    technologies: [
+      'html', 'css', 'javascript',
+    ],
+    liveDemo: 'https://rosemutai.github.io/My-Portfolio/',
+    sourceCode: 'https://rosemutai.github.io/My-Portfolio/'
+  },
+
+  {
+    name: 'Facebook 360',
+    experience: {
+      company: 'canopy',
+      role: 'Back End Dev',
+      year: 2015,
+    },
+    description: 'A daily selection of privately personalized reads no accounts or sign-ups required.',
+    featuredImage: './src/assets/SnapshootPortfolio2.svg',
+    technologies: [
+      'html', 'css', 'javascript',
+    ],
+    liveDemo: 'https://rosemutai.github.io/My-Portfolio/',
+    sourceCode: 'https://rosemutai.github.io/My-Portfolio/'
+  },
+
+  {
+    name: 'Uber Navigation',
+    experience: {
+      company: 'canopy',
+      role: 'Back End Dev',
+      year: 2015,
+    },
+    description: 'A daily selection of privately personalized reads no accounts or sign-ups required.',
+    featuredImage: './src/assets/SnapshootPortfolio3.svg',
+    technologies: [
+      'html', 'css', 'javascript',
+    ],
+    liveDemo: 'https://rosemutai.github.io/My-Portfolio/',
+    sourceCode: 'https://rosemutai.github.io/My-Portfolio/'
+  },
+];
+
+export default MyProjects;
