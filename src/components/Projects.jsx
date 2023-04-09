358616e7bd234f67b0e7cc64ed4695dafc272025
rosemutai <chepngetichrose2030@gmail.com>
2023-04-09 22:18:39 +0300

    Add a list of projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import MyProjects from '../../MyProjects'
+import SingleProject from './SingleProject';
+
+const Projects = () => {
+
+  const [projects, setprojects ] = useState([]);
+  
+  return (
+    <section className='my-projects px-12 md:px-48 w-full mt-8 bg-bodyColor'>
+      <h3 className='text-2xl text-navyBlue text-center font-bold my-3'>My Projects</h3>
+      <div className="projects md:grid md:grid-cols-3 gap-4">
+        {MyProjects.map((project, index) => (
+          <div key={index}>
+            {console.log(project)}
+            <SingleProject
+              name = {project.name}
+              featuredImage={project.featuredImage}
+              sourceCode = {project.sourceCode}
+              liveDemo = {project.liveDemo}
+              technologies={project.technologies}
+              description = {project.description}
+            />
+          </div>
+          
+        ))}
+
+      </div>
+
+    </section>
+  )
+}
+
+export default Projects
