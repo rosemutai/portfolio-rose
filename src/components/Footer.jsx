010d4bc4ca1bea9d4bed6914c51d58da801a94ef
rosemutai <chepngetichrose2030@gmail.com>
2023-04-12 21:59:27 +0300

    Save user form data

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Footer = () => {
+
+  const [fullname, setFullname] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const addContact = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = {fullname, email, message};
+      const docRef = await addDoc(collection(db, 'contacts'), {
+        contact: data
+      })
+      console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+      console.error("Error adding document: ", e);
+    }
+
+  }
+
   return (
     <div className='footer w-full h-full text-white md:flex md:flex-col
       md:items-center bg-contact-formbgmobile md:bg-contact-formbg pt-12
@@ -12,28 +34,47 @@ const Footer = () => {
         I’d love to help with it!
       </p>
 
-      <form action="" className="contact-form w-9/12 md:w-1/3 mx-auto">
+      <form onSubmit={addContact} className="contact-form w-9/12 md:w-1/3 mx-auto">
         <div className='w-full'>
           <label htmlFor="fullname"></label>
-          <input type="text" name="fullname" id="fullname" placeholder='Jane Doe'
-            className='w-full p-3 mb-6 rounded-md md:rounded-[2px] placeholder:text-[#B3BAC5] placeholder:text-base
-              focus:outline-none text-primary focus:shadow-md focus:shadow-primary'  
+          <input
+            type="text"
+            name="fullname"
+            id="fullname"
+            placeholder='Jane Doe'
+            className='w-full p-3 mb-6 rounded-md md:rounded-[2px] placeholder:text-[#B3BAC5] 
+              placeholder:text-base focus:outline-none text-primary focus:shadow-md focus:shadow-primary'
+            value={fullname}
+            onChange={(e) => setFullname(e.target.value)}
           />
         </div>
 
         <div className='w-full'>
           <label htmlFor="email"></label>
-          <input type="email" name="email" id="email" placeholder='janedoe@gmail.com' 
-            className='w-full p-3 mb-6 rounded-md md:rounded-[2px] placeholder:text-[#B3BAC5] placeholder:text-base
-              focus:outline-none text-primary'
+          <input
+            type="email" 
+            name="email" 
+            id="email" 
+            placeholder='janedoe@gmail.com' 
+            className='w-full p-3 mb-6 rounded-md md:rounded-[2px] placeholder:text-[#B3BAC5]
+              placeholder:text-base focus:outline-none text-primary'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
         <div className='w-full'>
           <label htmlFor="message"></label>
-          <textarea type="text" name="message" id="message" placeholder='Write your message here'
-            className='w-full rounded-md md:rounded-[2px] placeholder:text-[#B3BAC5] placeholder:text-base text-primary
-            px-3 focus:ring md:h-auto py-5 focus:outline-none focus:shadow focus:shadow-[#B3BAC5]'
+          <textarea
+            type="text"
+            name="message" 
+            id="message" 
+            placeholder='Write your message here'
+            className='w-full rounded-md md:rounded-[2px] placeholder:text-[#B3BAC5] placeholder:text-base 
+              text-primary px-3 focus:ring md:h-auto py-5 focus:outline-none focus:shadow focus:shadow-[#B3BAC5]'
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            
           ></textarea>
 
           <button type='submit' className='text-[#6070FF] py-2 px-5 md:mx-32 rounded-md border
