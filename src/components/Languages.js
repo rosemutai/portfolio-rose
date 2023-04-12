286584e3e6faf2baf9cf924cf5ea96bc2da104b7
rosemutai <chepngetichrose2030@gmail.com>
2023-04-12 18:09:45 +0300

    Add languages array

---
+++ src/components/Languages.js
@@ -0,0 +1,25 @@
+import js from '../assets/javascript.svg';
+import html from '../assets/html.svg';
+import css from '../assets/css.svg';
+
+const languages = [
+  {
+    imageUrl: html,
+    name: 'HTML',
+    style: 'shadow-E34F26',
+  },
+
+  {
+    imageUrl: css,
+    name: 'CSS',
+    style: 'shadow-0074D9',
+  },
+
+  {
+    imageUrl: js,
+    name: 'JavaSCript',
+    style: 'shadow-F7DF1E',
+  },
+]
+
+export default languages;
