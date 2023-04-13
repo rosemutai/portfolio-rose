22aa919ffde911f5bba194510aaa36527d7f11dc
rosemutai <chepngetichrose2030@gmail.com>
2023-04-13 21:43:36 +0300

    Add component ID

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
   const [projects, setprojects ] = useState([]);
   
   return (
-    <section className='my-projects px-12 md:px-48 w-full mt-8 bg-bodyColor'>
+    <section id="portfolio" className='my-projects px-12 md:px-48 w-full mt-8 bg-bodyColor'>
       <h3 className='text-2xl text-navyBlue text-center font-bold my-3'>My Projects</h3>
       <div className="projects md:grid md:grid-cols-3 gap-4">
         {MyProjects.map((project, index) => (
