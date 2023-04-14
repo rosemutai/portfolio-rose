8a3042b94582898aadd41a45b87bbf3c99124fc5
rosemutai <chepngetichrose2030@gmail.com>
2023-04-14 16:25:41 +0300

    Add project demo link

--- src/components/SingleProject.jsx
+++ src/components/SingleProject.jsx
@@ -19,15 +19,15 @@ function SingleProject({ name, experience, description, featuredImage, technolog
           </div>
        
          <h3 className='text-navyBlue font-bold text-xl mt-4 mb-0 md:mb-2'>{name}</h3>
-        <div className="tech flex my-2 md:absolute z-30 opacity-0 top-[50%] group-hover:opacity-100">
+        <div className="tech flex my-2 md:absolute z-30 opacity-0 top-[45%] group-hover:opacity-100">
           { techBtns }
         </div>
         <p className="description font-normal text-xs tracking-wide text-primary">{description}</p>
-        <div className='links flex justify-between mt-6'>
-          <a href={sourceCode} className='btn text-lightBlue py-2 px-6 rounded-md border border-lightBlue'>See Project
-          </a>
-          {/* <a href={sourceCode} className='underline mt-4 text-navyBlue font-semibold pb-3'>Source Code</a> */}
-          {/* <a href={liveDemo} className='underline mt-4 text-navyBlue font-semibold pb-3'>Demo</a> */}
+        <div className='links flex justify-between my-6 '>
+          <a href={sourceCode} className='btn text-secondary py-2 px-2 rounded-md border 
+            border-secondary'>Source Code</a>
+          <a href={liveDemo} className='demo border-x-0 border-t-0 border-b-2 mt-4 border-b-secondary
+            text-secondary font-semibold'>Demo</a>
         </div>
       </div>
     </div>
