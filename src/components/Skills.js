4816904075c22c8e12069288026062ac35b3dc16
rosemutai <chepngetichrose2030@gmail.com>
2023-06-04 17:10:00 +0300

    Update skills

---
+++ src/components/Skills.js
@@ -0,0 +1,22 @@
+const skills = [
+  {
+    name: 'Responsive Design',
+    style: 'shadow-E34F26',
+  },
+
+  {
+    name: 'GIT',
+    style: 'shadow-0074D9',
+  },
+
+  {
+    name: 'Testing',
+    style: 'shadow-F7DF1E',
+  },
+
+  {
+    name: 'Web performance optimization'
+  }
+]
+
+export default skills;
